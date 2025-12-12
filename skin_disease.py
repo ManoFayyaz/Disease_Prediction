6dcf08504e6a6e5c75a0b4e20d69ae00d44aef80
ManoFayyaz <mehnoorfayyaz06@gmail.com>
2025-12-12 15:33:17 +0500

    subheaders in app

--- skin_disease.py
+++ skin_disease.py
@@ -2,6 +2,11 @@ import streamlit as st
 import requests
 
 st.title('Skin Disease Detection Web-app')
+st.subheader('Upload the image of skin disease to predict')
+st.write("Please upload a clear image of the affected skin area for accurate prediction.")
+
+st.write('Major Skin Diseases Covered: Basal Cell Carcinoma, Melanoma, Actinic Keratoses, Benign Keratosis-like Lesions, Dermatofibroma, Melanocytic Nevi, Vascular Lesions')
+
 
 pic=st.file_uploader("Upload a picture of skin disease", type=["jpg","png","jpeg"])
 
