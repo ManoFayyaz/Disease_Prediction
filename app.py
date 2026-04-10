8a3df238ebbb71e7086c3e27d663f3fc5b5e3781
Mano Fayyaz <mehnoorfayyaz06@gmail.com>
2026-04-10 19:16:23 +0500

    removed hardcoded port

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from tensorflow.keras.models import load_model
 from PIL import Image
 import numpy as np
 import io
+import os
 
 app=Flask(__name__)
 
@@ -80,8 +81,9 @@ def predict():
    return jsonify({"prediction": result})
 
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
 
